6d1244e9f0c532bdddcaa6bff5f894608aa5a3f3
Mateusz Jankowski <peysone@me.com>
2018-11-25 14:23:01 +0100

    new string variable

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("GIT!");
-        for (int i = 10; i >=0 ; i--) {
-            if (i % 2==0)
-            System.out.println(i);
 
-        }
+        String zmiana;
      }
 }
+
 
