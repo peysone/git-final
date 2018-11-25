765ab0e25a27eb63ee66a4c4750ccc1b98d374bc
Mateusz Jankowski <peysone@me.com>
2018-11-25 13:52:50 +0100

    petla

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("GIT!");
-    }
+        for (int i = 10; i >=0 ; i--) {
+            if (i % 2==0)
+            System.out.println(i);
+
+        }
+     }
 }
+
